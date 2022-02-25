9a95f88453c903b6d7241c23d3bf14970c52ad0b
Juan De <>
2022-02-25 16:28:39 +0000

    spring cleaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-#function primecheck checks if a number is prime or not
+#function primecheck(number) checks if a number is prime or not
 def primecheck(number):
   for possiblefactors in range(2,math.isqrt(number)+1): 
     #loop through numbers from 2 to the sqrt of number
@@ -9,30 +9,12 @@ def primecheck(number):
   #assuming the number is only divisible by one and itself, it's prime
   return True
 
-'''
-def printfactors(factorlist):
-  noDuplicatesList = []
-  for factor in factorlist:
-      if factor not in noDuplicatesList:
-          noDuplicatesList.append(factor)
-  factorsstring=""
-  if len(factorlist)==1:
-    print(str(factorlist[0])+" * 1")
-  else:
-    for factor in range(len(noDuplicatesList)):
-      factorsstring+=str(noDuplicatesList[factor])+"^"+str(factorlist.count(noDuplicatesList[factor]))
-      #i concatenate the factor and how many of that factor was in the original list
-      if factor != len(noDuplicatesList)-1:
-        factorsstring+=" * "
-    print(factorsstring)
-'''
-########MAIN############
+'''#main'''
 inputNumber=int(input("Enter a positive integer: "))
-#print(primecheck(2))
-primefactors=[]
-factor=2
+primeFactors=[]
+factor=2 #factor is tarted at 2 
 initialNumber=inputNumber
-if initialNumber ==0:
+if initialNumber ==0: #test if an input has a special factorization
   print("Zero does not have a prime factorization")
 elif initialNumber==1:
   print("The only factor of 1 is 1")
@@ -42,28 +24,32 @@ else:
   #this code runs if a positive, non-zero integer is entered
 
   #this gathers every prime factor
-  while math.prod(primefactors) != initialNumber:
+  while math.prod(primeFactors) != initialNumber:
+  #i will keep dividing the initial numbers assuming the divisor is prime
+  # each prime divisor is counted as a prime factor in the list
     if primecheck(factor)==True:
       while inputNumber%factor==0:
         if inputNumber%factor==0:
-          primefactors.append(factor)
+          primeFactors.append(factor)
           inputNumber/=factor
     factor+=1
   
-
-  print("The factors are:")
   #this section prints the prime factors
-  noDuplicatesList = []
-  for factor in primefactors:
-      if factor not in noDuplicatesList:
-          noDuplicatesList.append(factor)
-  factorsstring=""
-  if len(primefactors)==1:
-    print(str(primefactors[0])+" * 1")
+  print("The factors are:")
+  noDuplicateFactors = [] #make a list without diplicates
+  for factor in primeFactors:
+      if factor not in noDuplicateFactors:
+          noDuplicateFactors.append(factor)
+
+  factorsString="" #initialize the organized output of the factors
+  if len(primeFactors)==1:
+    #just print out the factor * 1 if the number was already prime
+    print(str(primeFactors[0])+" * 1")
   else:
-    for factor in range(len(noDuplicatesList)):
-      factorsstring+=str(noDuplicatesList[factor])+"^"+str(primefactors.count(noDuplicatesList[factor]))
+    for factor in range(len(noDuplicateFactors)):
+      factorsString+=str(noDuplicateFactors[factor])+"^"+str(primeFactors.count(noDuplicateFactors[factor]))
       #i concatenate the factor and how many of that factor was in the original list
-      if factor != len(noDuplicatesList)-1:
-        factorsstring+=" * "
-    print(factorsstring)
+      if factor != len(noDuplicateFactors)-1:
+        #put an asterick for multiplication if this isnt the last factor
+        factorsString+=" * "
+    print(factorsString)
